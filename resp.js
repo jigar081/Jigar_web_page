672b4be471f7eae593fc4eba6aca6a16ab4aa8d6
JIGAR D PATEL <117442338+jigar081@users.noreply.github.com>
2023-04-11 10:10:46 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-resp');
+    nav-bar.classList.toggle('v-class-resp');
+    
+})
